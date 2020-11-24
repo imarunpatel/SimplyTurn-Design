f7af94362648649dd9c6d0ecca5bac7ad605303a
Arun Patel <arunpatel209@gmail.com>
2020-11-24 18:06:49 +0530

    Modified Server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.statuc(__dirname | '/dist/simplyturn-design'));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/simplyturn-design/index.html'));
-});
+app.use(express.static(__dirname + '/dist/simplyturn-design'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/simplyturn-design/index.html'));});
 app.listen(process.env.PORT || 8080);
